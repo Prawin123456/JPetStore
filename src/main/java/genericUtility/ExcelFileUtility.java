0849edb7cc367d277c7cc6eebcf124834ab533a6
kumar <kumar@Villain>
2025-08-11 17:14:53 +0530

    commit for (adding some methods in webdriverutility and excellfileutility

--- src/main/java/genericUtility/ExcelFileUtility.java
+++ src/main/java/genericUtility/ExcelFileUtility.java
@@ -1,5 +1,24 @@
 package genericUtility;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class ExcelFileUtility {
+	
+	public String toreaddataFromexcellFile(String Sheetname,int Row,int Cell) throws EncryptedDocumentException, IOException {
+		
+		FileInputStream fis= new FileInputStream("C:\\Users\\kumar\\git\\JPetStore\\new1.xlsx");
+		Workbook wb= WorkbookFactory.create(fis);
+		String Excell = wb.getSheet(Sheetname).getRow(Row).getCell(Cell).toString();
+		
+		
+		
+		return Excell;
+		
+	}
 
 }
--- src/main/java/genericUtility/WebDriverUtility.java
+++ src/main/java/genericUtility/WebDriverUtility.java
@@ -1,5 +1,59 @@
 package genericUtility;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
 public class WebDriverUtility {
+	
+	
+	public void ToMaxmise(WebDriver driver) {
+		driver.manage().window().maximize();
+		
+	}
+	public void tohandledropdownindex(int index,WebElement element) {
+		Select select= new Select(element);
+		select.selectByIndex(index);
+		
+	}
+	 public void tohandbyvisibletext(WebElement element,String value) {
+		 Select select= new Select(element);
+		 select.selectByValue(value);
+		 
+	 }
+	 public void tohandledropdownbyvisibletext(WebElement element,String text) {
+		 Select select= new Select(element);
+		 select.selectByValue(text);
+		 
+	 }
+	 public void tominimize(WebDriver driver) {
+		 driver.manage().window().minimize();
+	 }
+	 public void ToDragAndDrop(WebDriver driver,WebElement src,WebElement target) {
+			Actions action=new Actions(driver);
+			action.dragAndDrop(src, target).perform();
+		}
+	 public void ToDoubleClick(WebDriver driver,WebElement element) {
+			Actions action=new Actions(driver);
+			action.doubleClick(element).perform();
+		}
+		
+		public void ToRightClick(WebDriver driver,WebElement element) {
+			Actions action=new Actions(driver);
+			action.contextClick(element).perform();
+		}
+		
+		public void ToMoveToElement(WebDriver driver,WebElement element) {
+			Actions action=new Actions(driver);
+			action.moveToElement(element).perform();
+			
+		}
+		public void WaitForElement(WebDriver driver) {
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		}
+
 
 }
